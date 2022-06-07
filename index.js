55a5f51efb6d1cf6eb6dceddc8372607d8a6d2f1
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-06-07 00:12:41 +0000

    used a js object to reduce o(n)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const fib = (n) => {
-  if (n<=2) return 2;
-  return fib (n-1) + fib (n-2);
+// const fib = (n) => {
+//   if (n<=2) return 2;
+//   return fib (n-1) + fib (n-2);
+// };
+
+// using a js object to reduce o(n)
+const fib = (n,memo={}) => {
+  if (n in memo) return memo[n];
+  if (n<=2) return 1;
+  memo[n] = fib (n-1) + fib (n-2,memo);
+  return memo[n]
 };
 console.log (fib(6));
 console.log (fib(7));
