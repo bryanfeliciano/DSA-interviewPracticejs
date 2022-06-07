51889582be753aa1093f7b5dbd6518e7131a1607
bryanfeliciano <71857776+bryanfeliciano@users.noreply.github.com>
2022-06-07 23:00:40 +0000

    implementing more dsa practice in javascript

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //   return fib (n-1) + fib (n-2);
 // };
 
-// using a js object to reduce o(n)
+// using a js object to reduce o(n) from an exponential time complexity to a n time complexity
 const fib = (n,memo={}) => {
   if (n in memo) return memo[n];
   if (n<=2) return 1;
@@ -13,3 +13,12 @@ const fib = (n,memo={}) => {
 console.log (fib(6));
 console.log (fib(7));
 console.log (fib(8));
+
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+const addBinary = (a, b) => (a + b).toString(2);
+console.log(addBinary (10,20));
